28e9a630753049c04a714fae78669dd1c95c4d07
Habib Imron Cahyo Nugroho <habibimron1508@gmail.com>
2016-04-28 23:09:29 -0400

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,14 @@
 function printToPdf() {
 	var doc = new jsPDF();
+	
+	var specialElementHandlers = {
+	'.btn-dark': function(element, renderer){
+		return true;
+	}
+	};
 
 	doc.fromHTML($('body').get(0), 15, 15, {
 		'width': 170,
+		'elementHandlers': specialElementHandlers
 	});
 }
