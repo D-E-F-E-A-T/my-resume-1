8c514e36108f76035614e15a4bd7f112a53245e8
Habib Imron Cahyo Nugroho <habibimron1508@gmail.com>
2016-04-28 23:40:06 -0400

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,6 @@
 function printToPdf() {
-	var doc = new jsPDF();
-	
-	var specialElementHandlers = {
-	'.btn-dark': function(element, renderer){
-		return true;
-	}
-	};
-
-	doc.fromHTML($('body').get(0), 15, 15, {
-		'width': 170,
-		'elementHandlers': specialElementHandlers
+	var doc = new jsPDF('p', 'pt', 'letter');
+	doc.addHTML($('body')[0], function(){
+		doc.save(CV_Habib_Imron_Cahyo_Nugroho.pdf);
 	});
 }
